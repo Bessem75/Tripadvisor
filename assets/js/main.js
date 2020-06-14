75a3daae682d0e36a576e2758dea8c400e51b1cc
Bessem BOU-ZEID <bessem66@gmail.com>
2020-06-14 12:21:16 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,43 @@ const formidableMiddleware = require("express-formidable");
 const cors = require("cors");
 require("dotenv").config();
 
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+    const modal = $.querySelector(".modal");
+    const body = $.querySelector("body");
+
+    $.querySelector(".btn-signup").addEventListener("click", () => {
+
+        modal.classList.remove("hidden");
+
+        body.classList.add("disable");
+    });
+    $.querySelector(".icon-times").addEventListener("click", () => {
+
+        modal.classList.add("hidden");
+
+        body.classList.remove("disable");
+    });
+
+
+    $.querySelector(".btn-text-down").addEventListener("click", () => {
+        $.querySelector(".text").classList.toggle("overflow-hidden");
+        $.querySelector(".btn-text-down").classList.toggle("hidden");
+        $.querySelector(".btn-text-up").classList.toggle("hidden");
+    });
+
+
+    $.querySelector(".btn-text-up").addEventListener("click", () => {
+        $.querySelector(".text").classList.toggle("overflow-hidden");
+        $.querySelector(".btn-text-up").classList.toggle("hidden");
+        $.querySelector(".btn-text-down").classList.toggle("hidden");
+    });
+});
+
+
+
+
 const app = express();
 app.use(formidableMiddleware());
 app.use(cors());
@@ -66,40 +103,4 @@ app.all("*", (req, res) => {
 
 app.listen(process.env.PORT, () => {
     console.log("Server started");
-});
-
-
-
-const $ = document;
-
-$.addEventListener("DOMContentLoaded", () => {
-    const modal = $.querySelector(".modal");
-    const body = $.querySelector("body");
-
-    $.querySelector(".btn-signup").addEventListener("click", () => {
-
-        modal.classList.remove("hidden");
-
-        body.classList.add("disable");
-    });
-    $.querySelector(".icon-times").addEventListener("click", () => {
-
-        modal.classList.add("hidden");
-
-        body.classList.remove("disable");
-    });
-
-
-    $.querySelector(".btn-text-down").addEventListener("click", () => {
-        $.querySelector(".text").classList.toggle("overflow-hidden");
-        $.querySelector(".btn-text-down").classList.toggle("hidden");
-        $.querySelector(".btn-text-up").classList.toggle("hidden");
-    });
-
-
-    $.querySelector(".btn-text-up").addEventListener("click", () => {
-        $.querySelector(".text").classList.toggle("overflow-hidden");
-        $.querySelector(".btn-text-up").classList.toggle("hidden");
-        $.querySelector(".btn-text-down").classList.toggle("hidden");
-    });
 });
